691794cde627f10522ff8ca482d8648eaca6076c
Sean D Gillespie <sean@mistersg.net>
2025-12-04 17:03:30 -0500

    Add a Nix flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, haskellNix }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ haskellNix.overlay ];
+
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          inherit (haskellNix) config;
+        };
+
+        project = pkgs.haskell-nix.cabalProject' {
+          src = ./.;
+          compiler-nix-name = "ghc910";
+
+          shell = {
+            tools = {
+              cabal = {};
+              fourmolu = {};
+              hlint = {};
+              haskell-language-server = {};
+            };
+
+            buildInputs = with pkgs; [ nixpkgs-fmt ];
+          };
+        };
+
+        flake = project.flake { };
+
+      in
+        pkgs.lib.recursiveUpdate flake {
+          inherit project;
+
+          packages.default = flake.packages."hello-haskell:exe:hello-haskell";
+        });
+}
